9f8578cdc59d2ecbc8e2c785e3e8cfcbf357950f
VMiklos <vmiklos@frugalware.org>
2007-06-07 17:00:21 +0100

    sendmail.py: set dest to the frugalware-darcs list

--- sendmail/config.py
+++ sendmail/config.py
@@ -5,7 +5,7 @@ import os
 class config:
 	dir = os.path.join("_darcs", "third_party", "email")
 	latestfile = "latest"
-	dest = "vmiklos@frugalware.org"
+	dest = "frugalware-darcs@frugalware.org"
 	# just set this to None if you don't need this
 	darcsweb_url = "http://darcs.frugalware.org/darcsweb/darcsweb.cgi"
 	# if false, then the mail will be printed to stdout and no mail will be
