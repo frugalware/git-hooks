05b22e484e7862c26cf4c25ac7608f654a93b83b
VMiklos <vmiklos@frugalware.org>
2007-06-24 10:46:06 +0100

    don't try to read stin twice

--- git-hooks.py
+++ git-hooks.py
@@ -2,40 +2,38 @@
 
 import os, sys
 
-class Hook:
-	def __init__(self, callback):
+def run_hook(callback, old, new):
+	sock = os.popen("git rev-list %s..%s" % (old, new))
+	hashes = sock.readlines()
+	sock.close()
+	hashes.reverse()
 
-		# currently we don't care about !master branches
-		old = None
-		for i in sys.stdin.readlines():
-			(old, new, ref) = i.split(' ')
-			if ref == "refs/heads/master":
-				break
-		if not old:
-			return
+	for i in hashes:
+		callback(i.strip())
 
-		sock = os.popen("git rev-list %s..%s" % (old, new))
-		hashes = sock.readlines()
-		sock.close()
-		hashes.reverse()
+def read_stdin():
+	# currently we don't care about !master branches
+	old = None
+	for i in sys.stdin.readlines():
+		(old, new, ref) = i.split(' ')
+		if ref == "refs/heads/master":
+			break
+	if not old:
+		sys.exit(1)
+	return old, new
 
-		for i in hashes:
-			callback(i.strip())
 
 if __name__ == "__main__":
 	sys.path.append("/etc/git-hooks")
 	sys.path.append("/usr/share/git-hooks")
 	from config import config as myconfig
+	old, new = read_stdin()
 	for i in myconfig.enabled_plugins:
 		s = "%s.%s" % (i, i)
 		plugin = __import__(s)
 		for j in s.split(".")[1:]:
 			plugin = getattr(plugin, j)
-		s = "%s.config" % i
-		config = __import__(s)
-		for j in s.split(".")[1:]:
-			config = getattr(config, j)
 		try:
-			hook = Hook(plugin.callback)
+			run_hook(plugin.callback, old, new)
 		except Exception, s:
 			print "Error, probably connect to CIA timed out."
