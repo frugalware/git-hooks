936f49dfea8a1282e1ad426c5b4bae705c4704f4
VMiklos <vmiklos@frugalware.org>
2007-06-30 16:02:07 +0100

    cia: correct gitweb url

--- cia/config.py
+++ cia/config.py
@@ -5,7 +5,7 @@ import os
 class config:
 	project = "Frugalware"
 	# just set this to None if you don't need this
-	gitweb_url = "http://git.frugalware.org"
+	gitweb_url = "http://git.frugalware.org/gitweb/gitweb.cgi"
 	rpc_uri = "http://cia.vc"
 	# if false, then the mail will be printed to stdout and no xml-rpc post
 	# will be performed
