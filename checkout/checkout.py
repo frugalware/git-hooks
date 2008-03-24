d8103671c0116a3b7ef286fdc907ae562a796d31
Miklos Vajna <vmiklos@frugalware.org>
2008-03-24 14:34:42 +0100

    checkout: support for detached head

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -21,6 +21,13 @@ def callback(patch):
 			sys.stdout.flush()
 			time.sleep(1)
 		except OSError:
-			os.system("git checkout -f")
+			ret = os.system("git symbolic-ref HEAD &>/dev/null")
+			if ret != 0:
+				# this is a detached head
+				os.system("git checkout -m master")
+			else:
+				# we don't know where we are, need to
+				# check every checked out file
+				os.system("git checkout -f")
 			return
 	print "WARNING: Timeout exceeded, checkout failed!"
