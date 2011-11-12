aaa34772e8c8d2a30036812fdaea44d076359acf
Miklos Vajna <vmiklos@frugalware.org>
2011-11-12 16:23:00 +0100

    Revert "gitweb -> github"
    This reverts commit 35a0ac0b8c4ab7c8be9499c38242e3ef23606c56.

--- sendmail/config.py
+++ sendmail/config.py
@@ -5,7 +5,7 @@ import os
 class config:
 	dest = "frugalware-git@frugalware.org"
 	# just set this to None if you don't need this
-	gitweb_url = "https://github.com/frugalware"
+	gitweb_url = "http://git.frugalware.org/gitweb/gitweb.cgi"
 	# if false, then the mail will be printed to stdout and no mail will be
 	# sent
 	send = True
