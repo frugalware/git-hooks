87c5105b639066528563701160393576a8f61fd4
Miklos Vajna <vmiklos@frugalware.org>
2018-02-16 10:13:01 +0100

    disable sendmail plugin

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 
 class config:
 	enabled_plugins = {
-			"post-receive": ['sendmail', 'checkout', 'synchook', 'cia'],
+			# "post-receive": ['sendmail'],
+			"post-receive": ['checkout', 'synchook', 'cia'],
 			"pre-receive": ['detach']
 			}
