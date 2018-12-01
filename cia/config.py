0528d276ec7a80ee175d3da88e11c207210ecf77
Gabriel Craciunescu <nix.or.die@gmail.com>
2018-12-01 01:51:36 +0100

    fix mxw paths
    * mxw got own home now
    * set gitweb.cgi to None

--- cia/config.py
+++ cia/config.py
@@ -5,5 +5,5 @@ import os
 class config:
 	project = "Frugalware"
 	# just set this to None if you don't need this
-	gitweb_url = "http://git.frugalware.org/gitweb/gitweb.cgi"
-	sockpath = "/pub/other/mxw/mxw2.sock"
+	gitweb_url = "None"
+	sockpath = "/home/mxw/mxw/mxw2.sock"
